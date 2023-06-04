3cf3aea52342a124db2416a23a0b010f6aa73d60
Prajapati Kaushik <prajapatikaushik@gmail.com>
2023-06-04 13:04:12 +0100

    chore: symfony 6 compatibility #0000

--- example/metadata.php
+++ example/metadata.php
@@ -1,16 +1,22 @@
 <?php
 
+use Envoi\EnvChecker;
 use Envoi\Envoi;
+use Symfony\Component\Dotenv\Dotenv;
 
 include_once __DIR__.'/../vendor/autoload.php';
 
 // .env validate it.
+/*
 $envFilename = __DIR__.'/../.env';
 $envMetaFile = __DIR__.'/../.env.yaml';
 Envoi::init($envFilename, $envMetaFile);
+*/
+(new Dotenv(false))->loadEnv(__DIR__.'/../.env');
+// check the env!
+(new EnvChecker())->check(__DIR__.'/../.env.yaml');
 
-
-if (count($argv)!=2) {
+if (2 != count($argv)) {
     echo "Please pass 1 parameter: hashCode\n";
     exit(-1);
 }
